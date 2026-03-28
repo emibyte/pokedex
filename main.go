7be3f086ddb376172de8d7f57ae6034b9acbebe1
emibyte <fevymarine@gmail.com>
2026-03-28 18:10:29 +0100

    basic repl structure

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if scanner.Scan() {
+			text := scanner.Text()
+			cleanedInput := cleanInput(text)
+			if len(cleanedInput) == 0 {
+				continue
+			}
+			fmt.Printf("Your command was: %s\n", cleanedInput[0])
+		}
+	}
 }
