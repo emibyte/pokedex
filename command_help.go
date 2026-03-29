d07fc53b4530ae865bb4f995b26aec06bf7f76e9
emibyte <fevymarine@gmail.com>
2026-03-29 14:57:44 +0200

    pulling commands into seperate files

---
+++ command_help.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func commandHelp(_ *config) error {
+	var sb strings.Builder
+	beginning := "Welcome to the Pokedex!\nUsage:\n\n"
+	sb.WriteString(beginning)
+	for _, cmd := range registry.commands {
+		fmt.Fprintf(&sb, "%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println(sb.String())
+	return nil
+}
